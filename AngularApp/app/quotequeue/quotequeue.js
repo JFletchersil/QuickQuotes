d625b286f2ae844d67efa2fec6501e5201a6ffcf
Gmandam <Gmandam@mykolab.com>
2018-01-06 00:20:30 +0000

    App Update

--- AngularApp/app/quotequeue/quotequeue.js
+++ AngularApp/app/quotequeue/quotequeue.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('quoteTool.quotequeue', ['ngRoute'])
+angular.module('quoteTool.quotequeue', ['ngRoute', 'angularUtils.directives.dirPagination'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/quotequeue', {
@@ -8,7 +8,39 @@ angular.module('quoteTool.quotequeue', ['ngRoute'])
             controller: 'QuoteQueue'
         });
     }])
-
-    .controller('QuoteQueue', [function () {
-
+    
+    .controller('QuoteQueue', ['$scope', function ($scope) {
+        //$.material.init();
+        $scope.currentPage = 1;
+        $scope.quotes = [
+            {ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            {ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" },
+            { ReferenceNumber: "Test One", Type: "Test Two", Status: "Test Three", Age: "Test Four", Author: "Test Five" }
+        ]
+        //$scope.options = {
+        //    myFunction: function () { console.log('hello'); }
+        //};
+        //$scope.columns = [
+        //    {
+        //        id: 'foo',
+        //        key: 'foo',
+        //        classes: '',
+        //        lockWidth: true
+        //    },
+        //    {
+        //        id: 'bar',
+        //        key: 'bar',
+        //        classes: '',
+        //        lockWidth: true
+        //    }
+        //];
+        //$scope.rows = [{foo:"10", bar: "20"}]
     }]);
